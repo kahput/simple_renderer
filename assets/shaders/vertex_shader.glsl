54e71aa713aa14a2015384c9af322ff913ef6a67
kahput <kahputs@gmail.com>
2025-01-31 14:01:14 +0100

    chore(formatting): swap to right-hand pointer notation

--- assets/shaders/vertex_shader.glsl
+++ assets/shaders/vertex_shader.glsl
@@ -1,11 +1,11 @@
 #version 450 core
-layout(location = 0) in vec3 attrib_position;
-layout(location = 1) in vec2 attrib_uv;
+layout(location = 0) in vec3 a_position;
+layout(location = 1) in vec2 a_uv;
 
 out vec2 uv;
 uniform mat4 u_model, u_view, u_projection;
 
 void main() {
-    gl_Position = u_projection * u_view * u_model * vec4(attrib_position, 1.0);
-    uv = attrib_uv;
+    gl_Position = u_projection * u_view * u_model * vec4(a_position, 1.0);
+    uv = a_uv;
 };
